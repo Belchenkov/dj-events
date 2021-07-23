6e48e379c07e3e125d690bdabd44747e7567466c
Aleksey Belchenkov <u608110@gmail.com>
2021-07-23 17:48:09 +0400

    Head Tags & Layout

--- dj-events-frontend/pages/events/[slug].js
+++ dj-events-frontend/pages/events/[slug].js
@@ -1,11 +1,15 @@
-import React from 'react';
+import { useRouter } from "next/router";
 
-const EventPage = () => {
+import Layout from "../../components/Layout";
+
+export default function EventPage() {
+    const router = useRouter();
+
     return (
-        <div>
-            <h1>My Event 1</h1>
-        </div>
-    );
-};
-
-    export default EventPage;
+        <Layout>
+            <h1>My Event</h1>
+            <h3>{ router.query.slug }</h3>
+            <button onClick={() => router.push('/')}>Click</button>
+        </Layout>
+    )
+}
