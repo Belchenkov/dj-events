db2fe352a77e253418218ce78128f62c7723c59f
Aleksey Belchenkov <u608110@gmail.com>
2021-08-06 18:58:20 +0400

    Install strapi & connect to cloudinary

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd75102a5adcc5071f9fc4dcfc12c483'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5f084b54-0973-44d7-9477-cb4f745ebe2b'
+};
